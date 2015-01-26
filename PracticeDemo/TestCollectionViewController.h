ab848573be88cfbfe76e039d7bc6a32d1c6fc589
345 <peichenguang@chinamobo.com>
2015-01-26 19:38:52 +0800

    小的修改

---
+++ PracticeDemo/TestCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestCollectionViewController.h
+//  PracticeDemo
+//
+//  Created by 345 on 15/1/16.
+//  Copyright (c) 2015年 345. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestCollectionViewController : UICollectionViewController
+
+@end
