505ce1472b8d66eecc623acd98c47aa340e5803e
345 <peichenguang@chinamobo.com>
2015-02-09 10:03:44 +0800

    增加图片的使用示例

--- Corp/Corp/ViewController.m
+++ Corp/Corp/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "CGCropImage.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
 {
@@ -21,13 +22,32 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-//    CGZoomScrollView *cropImage= [[CGZoomScrollView alloc] initWithFrame:self.view.bounds];
-    CGCropImage *cropImage= [[CGCropImage alloc] initWithFrame:self.view.bounds];
-//    cropImage.isAutoImageViewContentMode = YES;
-    cropImage.image = [UIImage imageNamed:@"1.jpg"];
-    self.view = cropImage;
+//    CGCropImage *cropImage= [[CGCropImage alloc] initWithFrame:self.view.bounds];
+////    cropImage.isAutoImageViewContentMode = YES;
+//    cropImage.image = [UIImage imageNamed:@"1.jpg"];
+//    self.view = cropImage;
     
+    CALayer *testImageLayer = [CALayer layer];
+    testImageLayer.frame = CGRectMake(0, 0, 200, 180);
+    testImageLayer.position = self.view.center;
     
+    [self.view.layer addSublayer:testImageLayer];
+    
+    UIImage *image = [UIImage imageNamed:@"1.jpg"];
+    
+    //设置为图层添加图片，桥接CGImage使其满足类型需求
+    testImageLayer.contents = (__bridge id)image.CGImage;
+    
+    //设置图层图片的显示方式
+    testImageLayer.contentsGravity = kCAGravityResizeAspect;//kCAGravityCenter;
+    
+    //表示每个点多少像素点,当contentsGravity 属性设置拉伸图片的属性值时无效, 例如：kCAGravityResizeAspect属性
+//    testImageLayer.contentsScale = [UIScreen mainScreen].scale;
+    
+    //设置图片的显示区域内容，这里和frame不一样，是相对于图片来说
+    testImageLayer.contentsRect = CGRectMake(0, 0, 1, 1);
+    
+    testImageLayer.backgroundColor = [UIColor blueColor].CGColor;
 }
 
 - (void)didReceiveMemoryWarning {
